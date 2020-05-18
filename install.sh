eaef56db5d704a5fbe8ca07c471b39b83def7ba5
Dan <dan.girsh@gmail.com>
2020-05-17 18:34:18 -0700

    local install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+
+NEUROSYS_HOME="$(dirname "$(readlink -f "$0")")"/home
+
+# # rsync -Pav --rsync-path="sudo rsync" nixos/ /etc/nixos/
+
+# Cleanup old symlinks
+rm -f $HOME/.doom.d
+rm -f $HOME/.xmonad/xmonad.hs
+
+ln -s $NEUROSYS_HOME/.doom.d $HOME/.doom.d
+ln -s $NEUROSYS_HOME/.xmonad/xmonad.hs $HOME/.xmonad/xmonad.hs
